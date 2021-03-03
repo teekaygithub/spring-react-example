b4db2af18cb14e5878aaa00ab8fdadcc5228130c
teekaygithub <tomoyuki.kato@yahoo.com>
2021-03-02 17:47:36 -0800

    initial commit

---
+++ src/main/java/com/example/springreact/course/CourseResource.java
@@ -0,0 +1,20 @@
+package com.example.springreact.course;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseResource {
+
+  @Autowired
+  private CoursesHardcodedService courseManagementService;
+
+  @GetMapping("/instructors/{username}/courses")
+  public List<Course> getAllCourses(@PathVariable String username) {
+    return courseManagementService.findAll();
+  }
+}
